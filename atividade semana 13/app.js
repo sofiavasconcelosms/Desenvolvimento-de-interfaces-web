bf577fb4655340521233d9a9dbf8e9f74caa41d7
Sofia Vasconcelos Moreira e Silva <sofiavasconcelosmsilva@gmail.com>
2024-06-18 18:05:44 -0300

    Add files via upload

---
+++ atividade semana 13/app.js
@@ -0,0 +1,7 @@
+let alunos = [
+    { nome: "João Silva", matricula: "2021001", curso: "Engenharia de Computação", turma: "1" },
+    { nome: "Maria Santos", matricula: "2021002", curso: "Ciência da Computação", turma: "8" },
+    { nome: "Pedro Oliveira", matricula: "2021003", curso: "Sistemas de Informação", turma: "3" },
+    { nome: "Ana Souza", matricula: "2021004", curso: "Engenharia Elétrica", turma: "4" },
+    { nome: "Carlos Costa", matricula: "2021005", curso: "Engenharia Civil", turma: "6" }
+];
