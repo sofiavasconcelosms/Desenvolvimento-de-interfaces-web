428095805dd50b34af4aee9683d7fc5ec46b4238
Sofia Vasconcelos Moreira e Silva <sofiavasconcelosmsilva@gmail.com>
2024-06-18 18:06:56 -0300

    Add files via upload

---
+++ aula DOM/app.js
@@ -0,0 +1,11 @@
+//let cidades = [
+//    { "id": 1, "cidade": "Belo Horizonte", "estado": "MG", "populacao": 3800000},
+//    { "id": 2, "cidade": "Sao Paulo", "estado": "SP", "populacao": 11800000},
+//    { "id": 3, "cidade": "Rio de Janeiro", "estado": "RJ", "populacao": 5300000},
+//    { "id": 4, "cidade": "Curitiba", "estado": "PR", "populacao": 1230000},
+//    { "id": 5, "cidade": "Fortaleza", "estado": "CE", "populacao": 100000}
+// ]
+
+const urlCidades =''
+
+
